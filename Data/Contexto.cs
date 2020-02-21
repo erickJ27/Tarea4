e175327566f27ea7cc232aabca486a131ffcc344
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-02-21 10:22:25 -0300

    Contexto creado

---
+++ Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaUniversitario.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlite(@"Data source=Database/SistemaUniversitario.db"));
+        }
+    }
+}
