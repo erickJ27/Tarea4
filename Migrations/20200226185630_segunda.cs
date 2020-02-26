20d5f38903f01656a49cc40ad8aaaf6115d2bdd6
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-02-26 16:21:23 -0300

    se crearon los registro

---
+++ Migrations/20200226185630_segunda.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SistemaUniversitario.Migrations
+{
+    public partial class segunda : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Asginatura",
+                columns: table => new
+                {
+                    AsignaturaId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Codigo = table.Column<string>(nullable: true),
+                    Descripcion = table.Column<string>(nullable: true),
+                    PreRequisito = table.Column<string>(nullable: true),
+                    Creditos = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Asginatura", x => x.AsignaturaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Estudiante",
+                columns: table => new
+                {
+                    EstudianteId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Matricula = table.Column<string>(nullable: true),
+                    Nombres = table.Column<string>(nullable: true),
+                    Balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudiante", x => x.EstudianteId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inscripcion",
+                columns: table => new
+                {
+                    InscripcionId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Semestre = table.Column<string>(nullable: true),
+                    Limite = table.Column<int>(nullable: false),
+                    Tomados = table.Column<int>(nullable: false),
+                    Disponibles = table.Column<int>(nullable: false),
+                    EstudianteId = table.Column<int>(nullable: false),
+                    Monto = table.Column<decimal>(nullable: false),
+                    Balance = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inscripcion", x => x.InscripcionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pago",
+                columns: table => new
+                {
+                    PagoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    InscripcionId = table.Column<int>(nullable: false),
+                    Monto = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pago", x => x.PagoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Asginatura");
+
+            migrationBuilder.DropTable(
+                name: "Estudiante");
+
+            migrationBuilder.DropTable(
+                name: "Inscripcion");
+
+            migrationBuilder.DropTable(
+                name: "Pago");
+        }
+    }
+}
