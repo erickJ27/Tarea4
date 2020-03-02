c8e9d828bc0c7259048504df02cc8edf1c208ba9
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-03-02 17:48:44 -0300

    Cambios

---
+++ Models/InscripcionesDetalle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaUniversitario.Models
+{
+    public class InscripcionesDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        public int InscripcionId { get; set; }
+
+
+    }
+}
