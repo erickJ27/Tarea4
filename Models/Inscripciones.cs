9e0344b5f01c13ef89b088e4690c8401ac5b7509
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-03-04 17:42:51 -0300

    subiendo

--- Models/Inscripciones.cs
+++ Models/Inscripciones.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -25,6 +26,8 @@ namespace SistemaUniversitario.Models
         public decimal Monto { get; set; }
         public decimal Balance { get; set; }
 
+        [ForeignKey("InscripcionId")]
+        public List<InscripcionesDetalle> Detalles { get; set; }
         public Inscripciones()
         {
             InscripcionId = 0;
@@ -36,6 +39,7 @@ namespace SistemaUniversitario.Models
             EstudianteId = 0;
             Monto = 0;
             Balance = 0;
+            Detalles = new List<InscripcionesDetalle>();
         }
     }
 
--- Models/InscripcionesDetalle.cs
+++ Models/InscripcionesDetalle.cs
@@ -11,7 +11,19 @@ namespace SistemaUniversitario.Models
         [Key]
         public int DetalleId { get; set; }
         public int InscripcionId { get; set; }
+        public int AsignaturaId { get; set; }
+        public string DescripcionAsignatura { get; set; }
+        public int Creditos { get; set; }
+        public decimal SubTotal { get; set; }
 
-
+        public InscripcionesDetalle()
+        {
+            DetalleId = 0;
+            InscripcionId = 0;
+            AsignaturaId = 0;
+            DescripcionAsignatura = string.Empty;
+            Creditos = 0;
+            SubTotal = 0;
+        }
     }
 }
